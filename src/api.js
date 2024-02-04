cc8fc96d062ab1242798d4de25a7779dd1d8d4f1
Aditya Soni <adityasoni971@gmail.com>
2024-02-04 21:49:19 +0530

    initial update

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '18a299f1b9msh4f8217070110032p1ab44djsn60e72292ec03',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "4081df587567d18513e9d697e43b8a8d";
